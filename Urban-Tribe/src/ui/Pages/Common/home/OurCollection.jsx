b88d74f1d00065d72e6f103787eb579da345314a
riyapatel1610 <patelriya82000@gmail.com>
2024-04-10 10:37:50 +0530

    sucess

--- Urban-Tribe/src/ui/Pages/Common/home/OurCollection.jsx
+++ Urban-Tribe/src/ui/Pages/Common/home/OurCollection.jsx
@@ -15,8 +15,8 @@ const data = [{
   "image": i1_img,
   "old_price": 600,
   "rating": 4,
-  "discount":"30%"
-  
+  "discount": "30%"
+
 },
 {
   "id": 2,
@@ -26,7 +26,7 @@ const data = [{
   "image": i2_img,
   "old_price": 3233.00,
   "rating": 5,
-  "discount":"14%"
+  "discount": "14%"
 },
 {
   "id": 4,
@@ -36,7 +36,7 @@ const data = [{
   "image": i3_img,
   "old_price": 2899.00,
   "rating": 4,
-  "discount":"10%"
+  "discount": "10%"
 },
 {
   "id": 4,
@@ -46,7 +46,7 @@ const data = [{
   "image": i4_img,
   "old_price": 1850.00,
   "rating": 4,
-  "discount":"35%"
+  "discount": "35%"
 },
 {
   "id": 5,
@@ -56,7 +56,7 @@ const data = [{
   "image": i5_img,
   "old_price": 2399.00,
   "rating": 5,
-  "discount":"20%"
+  "discount": "20%"
 }
 ]
 export default function OurCollection() {
@@ -98,48 +98,50 @@ export default function OurCollection() {
   return (
     <div className=' flex flex-col justify-center items-center px-4 lg:px-20 lg:py-20 gap-6'>
       <div className='w-full h-fit'>
-      <Slider {...settings}>
-      {
-        data.map((item, index) => (
-          <div id='slider-boxes' key={index} className='bg-white  flex flex-col justify-center items-center '>
-            {/* image box css */}
-            <div id='image-box' className='overflow-hidden relative group'>
-            {
-              
-                <img src={item.image} alt={item.title} className='w-full h-fit object-cover' />
+        <Slider {...settings}>
+          {
+            data.map((item, index) => (
+              <div id='slider-boxes' key={index} className='bg-white  flex flex-col justify-center items-center '>
+                {/* image box css */}
+                <div id='image-box' className='overflow-hidden relative group'>
+                  {
 
-            }
-            {/* <div className='absolute h-full w-full bg-black/20 flex items-center justify-center -bottom-[5.5rem] group-hover:bottom-0 opacity-0 group-hover:opacity-100 transition-all duration-300'>
+                    <img src={item.image} alt={item.title} className='w-full h-fit object-cover' />
+
+                  }
+                  {/* <div className='absolute h-full w-full bg-black/20 flex items-center justify-center -bottom-[5.5rem] group-hover:bottom-0 opacity-0 group-hover:opacity-100 transition-all duration-300'>
 
             </div> */}
-            <div className='flex gap-2 mt-1 absolute -bottom-[-395px]  left-6...'>
-            <h7 className='bg-red-600 px-1.5 py-0.5 rounded-md text-xs text-white'>sale{item.discount}</h7>
-            </div>
-            </div>
-            <div className='flex flex-col justify-center text-black '>
-             <h7>{item.rating}</h7>
-            </div>
-            <div className='flex flex-col justify-center overflow-hidden'>
-              <h6 className='text-black'>{item.title}</h6>
-            </div>
-            <div className='flex flex-col justify-center  '>
-              <h6 className='text-black '>Rs.{item.price}</h6>
-            </div>
-            <div className='flex flex-col justify-center gap-2 mt-1'>
-              <h6 className='text-black text-sm line-through opacity-50'>{item.old_price}</h6>
-            </div>
-            
-          </div>
+                  <div className='flex gap-1 mt-1 absolute -bottom-[-395px]  left-6...'>
+                    <h7 className='bg-red-600 px-1.5 py-0.5 rounded-md text-xs text-white'>sale{item.discount}</h7>
+                  </div>
+                </div>
+                <div className='text-black m-0'>
+                  <h7>{item.rating}</h7>
+                </div>
+                <div className='overflow-hidden m-0'>
+                  <h6 className='text-black'>{item.title}</h6>
+                </div>
+                <div class="flex items-center m-0">
+                <div className=''>
+                  <h6 className='text-black '>Rs.{item.price}</h6>
+                </div>
+                <div className=''>
+                  <h6 className='text-black text-sm line-through opacity-50'>{item.old_price}</h6>
+                </div>
 
-        ))
-        
-      }
-      </Slider>
-      
-     
+                </div>
+              </div>
+
+            ))
+
+          }
+        </Slider>
+
+
       </div>
-     
-     
+
+
     </div>
   )
 }
